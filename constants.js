6ec61cd1962842a3d07c10139eef5f9db0b08b23
Artemides <edmpulasky@gmail.com>
2023-05-25 11:18:25 -0500

    create wait for transaction function

---
+++ constants.js
@@ -0,0 +1,146 @@
+export const contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+
+export const abi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "agregatorAddress",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "NotEnoughDonation",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "NotOwner",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "WithdrawFailed",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [],
+    name: "FoundsWithdrawn",
+    type: "event",
+  },
+  {
+    stateMutability: "payable",
+    type: "fallback",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_founder",
+        type: "address",
+      },
+    ],
+    name: "founderExists",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "fund",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getAgregator",
+    outputs: [
+      {
+        internalType: "contract AggregatorV3Interface",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "founder",
+        type: "address",
+      },
+    ],
+    name: "getFounderFounds",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "uint256",
+            name: "totalFunds",
+            type: "uint256",
+          },
+          {
+            internalType: "uint256",
+            name: "avaiableFund",
+            type: "uint256",
+          },
+        ],
+        internalType: "struct FundMe.FundObj",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getOwner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdraw",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "withdrawCheaper",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    stateMutability: "payable",
+    type: "receive",
+  },
+];
--- index.js
+++ index.js
@@ -1,8 +1,46 @@
-const connect = async () => {
+import { abi, contractAddress } from "./constants.js";
+import { ethers } from "./ethers-5.2.esm.min.js";
+
+const connectButton = document.getElementById("wallet-connect");
+const fundButton = document.getElementById("wallet-fund");
+connectButton.onclick = connect;
+fundButton.onclick = fund;
+
+async function fund() {
+  const ethereum = window.ethereum;
+  if (!ethereum) return;
+  const ethAmmount = "0.1";
+  const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const fundMeContract = new ethers.Contract(contractAddress, abi, signer);
+
+  try {
+    const transactionResponse = await fundMeContract.fund({
+      value: ethers.utils.parseEther(ethAmmount),
+    });
+    await waitForTransactionToBeMined(transactionResponse, provider);
+    console.log("done!");
+    //listen to event
+  } catch (error) {
+    console.log({ error });
+  }
+}
+
+async function connect() {
   const ethereum = window.ethereum;
   if (!ethereum) return;
 
   await ethereum.request({ method: "eth_requestAccounts" });
   const account = await ethereum.request({ method: "eth_accounts" });
-  document.getElementById("wallet-connect").innerHTML = account[0];
-};
+  connectButton.innerHTML = account[0];
+}
+
+function waitForTransactionToBeMined(transactionResponse, provider) {
+  console.log(`Minig TX: ${transactionResponse.hash}`);
+  return new Promise((resolve, reject) => {
+    provider.once(transactionResponse.hash, (transactionReceipt) => {
+      console.log(`Completed with ${transactionReceipt.confirmations}`);
+    });
+    resolve();
+  });
+}
